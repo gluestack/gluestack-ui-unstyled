e86740af8539d2998225cebac60efb0dcc8e66e1
Damini <daminipandey1310@gmail.com>
2023-08-25 19:08:41 +0530

    feat: add linear-gradient component

--- example/storybook/src/components/Forms/LinearGradient/LinearGradient.tsx
+++ example/storybook/src/components/Forms/LinearGradient/LinearGradient.tsx
@@ -1,19 +1,37 @@
 import type { ComponentStory } from '@storybook/react-native';
 import React from 'react';
-import { Box } from '../../../ui-components';
-
-import { LinearGradient } from '../../../ui-components';
+import {
+  Box,
+  Text,
+  LinearGradient,
+  Button,
+  ButtonIcon,
+  Icon,
+  EditIcon,
+  Pressable,
+} from '../../../ui-components';
 
 type MyLinearGradientStory = ComponentStory<typeof LinearGradient>;
 
 const LinearGradientStory: MyLinearGradientStory = ({ ...props }: any) => {
   return (
-    <LinearGradient {...props} colors={['rgba(0,0,0,0.8)', 'transparent']}>
-      <Box>ddnb</Box>
+    <LinearGradient {...props} colors={['$purple400', '$red400']}>
+      <Box px={'$12'} py={'$6'}>
+        <Text color="$white">Box</Text>
+      </Box>
     </LinearGradient>
   );
 };
 
 export default LinearGradientStory;
 
-export { LinearGradient };
+export {
+  LinearGradient,
+  Box,
+  Text,
+  Button,
+  ButtonIcon,
+  Icon,
+  EditIcon,
+  Pressable,
+};
