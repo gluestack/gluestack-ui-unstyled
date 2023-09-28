fae495bdea916bdaa8ec6344c54ba5f45b9abc46
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-09-28 14:15:49 +0530

    fix: component typing improvements

--- example/storybook/src/ui-components/Badge/index.tsx
+++ example/storybook/src/ui-components/Badge/index.tsx
@@ -8,9 +8,11 @@ type RootProps = React.ComponentProps<typeof Root>;
 type IconProps = React.ComponentProps<typeof Icon>;
 type TextProps = React.ComponentProps<typeof Text>;
 
-type IBadgeComponentType = ((props: RootProps) => JSX.Element) & {
-  Icon: (props: IconProps) => JSX.Element;
-  Text: (props: TextProps) => JSX.Element;
+type IBadgeComponentType = React.ForwardRefExoticComponent<
+  (props: RootProps) => JSX.Element
+> & {
+  Icon: React.ForwardRefExoticComponent<(props: IconProps) => JSX.Element>;
+  Text: React.ForwardRefExoticComponent<(props: TextProps) => JSX.Element>;
 };
 
 const BadgeMain = Badge as IBadgeComponentType;
