ea5c0cc01e6f4a8f62667bb197e118afadaf3af6
Damini <daminipandey1310@gmail.com>
2023-11-27 08:19:47 +0530

    feat: initial commit

--- example/storybook/src/components/DataDisplay/Accordion/Accordion.tsx
+++ example/storybook/src/components/DataDisplay/Accordion/Accordion.tsx
@@ -16,134 +16,269 @@ type MyAccordionStory = ComponentStory<typeof Accordion>;
 
 const AccordionStory: MyAccordionStory = ({}: any) => {
   return (
-    <Accordion
-      alignSelf="center"
-      type="multiple"
-      isCollapsible={true}
-      variant={'filled'}
-      size="sm"
-      mt="$5"
-    >
-      <AccordionItem value="a" isDisabled={true}>
-        <AccordionHeader>
-          <AccordionTrigger>
-            {({ isExpanded }: { isExpanded: boolean }) => {
-              return (
-                <>
-                  <AccordionTitleText>
-                    How do I place an order?
-                  </AccordionTitleText>
-                  {isExpanded ? (
-                    <AccordionIcon as={ChevronUpIcon} />
-                  ) : (
-                    <AccordionIcon as={ChevronDownIcon} />
-                  )}
-                </>
-              );
-            }}
-          </AccordionTrigger>
-        </AccordionHeader>
-        <AccordionContent>
-          <AccordionContentText>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, sed
-            laudantium eligendi maxime rerum, saepe vitae unde voluptas hic,
-            culpa ex dolorem omnis incidunt quisquam? Ex fuga debitis recusandae
-            incidunt.
-          </AccordionContentText>
-        </AccordionContent>
-      </AccordionItem>
+    <>
+      {' '}
+      <Accordion
+        alignSelf="center"
+        type="multiple"
+        isCollapsible={true}
+        variant={'filled'}
+        size="sm"
+        mt="$5"
+        // value={['c', 'd']}
+        // isDisabled={true}
+      >
+        <AccordionItem value="a">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      How do I place an order?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, sed
+              laudantium eligendi maxime rerum, saepe vitae unde voluptas hic,
+              culpa ex dolorem omnis incidunt quisquam? Ex fuga debitis
+              recusandae incidunt.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
 
-      <Divider />
+        <Divider />
 
-      <AccordionItem value="b">
-        <AccordionHeader>
-          <AccordionTrigger>
-            {({ isExpanded }: { isExpanded: boolean }) => {
-              return (
-                <>
-                  <AccordionTitleText>
-                    What payment methods do you accept?
-                  </AccordionTitleText>
-                  {isExpanded ? (
-                    <AccordionIcon as={ChevronUpIcon} />
-                  ) : (
-                    <AccordionIcon as={ChevronDownIcon} />
-                  )}
-                </>
-              );
-            }}
-          </AccordionTrigger>
-        </AccordionHeader>
-        <AccordionContent>
-          <AccordionContentText>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-            maxime modi quaerat temporibus quos, omnis, vel ullam debitis labore
-            error fugit, blanditiis dolore illum consequuntur laboriosam.
-            Voluptates est obcaecati nemo!
-          </AccordionContentText>
-        </AccordionContent>
-      </AccordionItem>
+        <AccordionItem value="b">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      What payment methods do you accept?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis maxime modi quaerat temporibus quos, omnis, vel ullam
+              debitis labore error fugit, blanditiis dolore illum consequuntur
+              laboriosam. Voluptates est obcaecati nemo!
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
 
-      <Divider />
+        <Divider />
 
-      <AccordionItem value="c">
-        <AccordionHeader>
-          <AccordionTrigger>
-            {({ isExpanded }: { isExpanded: boolean }) => {
-              return (
-                <>
-                  <AccordionTitleText>
-                    What payment methods do you accept?
-                  </AccordionTitleText>
-                  {isExpanded ? (
-                    <AccordionIcon as={ChevronUpIcon} />
-                  ) : (
-                    <AccordionIcon as={ChevronDownIcon} />
-                  )}
-                </>
-              );
-            }}
-          </AccordionTrigger>
-        </AccordionHeader>
-        <AccordionContent>
-          <AccordionContentText>
-            We accept all major credit cards, including Visa, Mastercard, and
-            American Express. We also support payments through PayPal.
-          </AccordionContentText>
-        </AccordionContent>
-      </AccordionItem>
+        <AccordionItem value="c">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      What payment methods do you accept?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              We accept all major credit cards, including Visa, Mastercard, and
+              American Express. We also support payments through PayPal.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
 
-      <Divider />
+        <Divider />
 
-      <AccordionItem value="d">
-        <AccordionHeader>
-          <AccordionTrigger>
-            {({ isExpanded }: { isExpanded: boolean }) => {
-              return (
-                <>
-                  <AccordionTitleText>
-                    Can I cancel my subscription at any time?
-                  </AccordionTitleText>
-                  {isExpanded ? (
-                    <AccordionIcon as={ChevronUpIcon} />
-                  ) : (
-                    <AccordionIcon as={ChevronDownIcon} />
-                  )}
-                </>
-              );
-            }}
-          </AccordionTrigger>
-        </AccordionHeader>
-        <AccordionContent>
-          <AccordionContentText>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorem
-            eos ex officia ratione omnis similique delectus aliquam culpa. Minus
-            quis numquam laboriosam non natus, distinctio facilis aspernatur
-            beatae earum.
-          </AccordionContentText>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+        <AccordionItem value="d">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      Can I cancel my subscription at any time?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              dolorem eos ex officia ratione omnis similique delectus aliquam
+              culpa. Minus quis numquam laboriosam non natus, distinctio facilis
+              aspernatur beatae earum.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <Accordion
+        alignSelf="center"
+        type="multiple"
+        isCollapsible={true}
+        variant={'filled'}
+        size="sm"
+        mt="$5"
+        // value={['c', 'd']}
+        // isDisabled={true}
+      >
+        <AccordionItem value="a1">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      How do I place an order?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, sed
+              laudantium eligendi maxime rerum, saepe vitae unde voluptas hic,
+              culpa ex dolorem omnis incidunt quisquam? Ex fuga debitis
+              recusandae incidunt.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+
+        <Divider />
+
+        <AccordionItem value="b1">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      What payment methods do you accept?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis maxime modi quaerat temporibus quos, omnis, vel ullam
+              debitis labore error fugit, blanditiis dolore illum consequuntur
+              laboriosam. Voluptates est obcaecati nemo!
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+
+        <Divider />
+
+        <AccordionItem value="c1">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      What payment methods do you accept?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              We accept all major credit cards, including Visa, Mastercard, and
+              American Express. We also support payments through PayPal.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+
+        <Divider />
+
+        <AccordionItem value="d1">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }: { isExpanded: boolean }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      Can I cancel my subscription at any time?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              dolorem eos ex officia ratione omnis similique delectus aliquam
+              culpa. Minus quis numquam laboriosam non natus, distinctio facilis
+              aspernatur beatae earum.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
   );
 };
 
